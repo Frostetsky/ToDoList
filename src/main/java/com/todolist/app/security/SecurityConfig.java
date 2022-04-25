e9df460a8ca04d2bbae6402a3b5145ae8f9883e1
Nikita <nik.nesterenko1999@bk.ru>
2022-04-25 22:44:28 +0300

    ToDoList | конфигурация Spring Security

---
+++ src/main/java/com/todolist/app/security/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.todolist.app.security;
+
+import com.todolist.app.model.Role;
+import com.todolist.app.service.UserService;
+import com.todolist.app.util.Constants;
+import com.todolist.app.util.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import static com.todolist.app.util.Constants.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserService userDetailsService;
+
+    private Environment environment;
+
+    private LogoutSuccessHandler logoutSuccessHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String[] anonymousPaths = environment.getProperty(Keys.ANONYMOUS_PATHS, String[].class);
+        String[] adminPaths = environment.getProperty(Keys.ADMIN_PATHS, String[].class);
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                //Доступ только для не зарегистрированных пользователей
+                .antMatchers(anonymousPaths).not().fullyAuthenticated()
+                //Доступ только для пользователей с ролью администратора системы
+                .antMatchers(adminPaths).hasRole(Role.ROLE_ADMIN.name().replaceAll(PREFIX_ROLE, EMPTY))
+                //Все остальные страницы требуют аутентификации
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessHandler(logoutSuccessHandler)
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Autowired
+    public void setUserDetailsService(UserService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Autowired
+    public void setLogoutSuccessHandler(LogoutSuccessHandler logoutSuccessHandler) {
+        this.logoutSuccessHandler = logoutSuccessHandler;
+    }
+}
